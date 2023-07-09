55780f0e6c15f340d43b7dffbb38c62f01e0d975
Uday <uday20_ug@cse.nits.ac.in>
2023-07-09 21:44:53 +0530

    fix1

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,64 @@
+// import express from 'express';
+// import Post from '../mongodb/models/post.js';
+
+
+// const router = express.Router();
+
+// router.route('/').get(async (req, res) => {
+//   try {
+//     const posts = await Post.find({});
+//     res.status(200).json({ success: true, data: posts });
+//   } catch (err) {
+//     res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });
+//   }
+// });
+
+// router.route('/').post(async (req, res) => {
+//   try {
+//     const { name, prompt, photo } = req.body;
+//     const formData = new FormData();
+//     formData.append("file", photo);
+//     formData.append("upload_preset", "aiimages");
+
+//     const response = await fetch(
+//       "https://api.cloudinary.com/v1_1/danlppyav/image/upload",
+//       {
+//         method: "POST",
+//         body: formData,
+//       }
+//     );
+
+//     const data = await response.json();
+//     const imageUrl = data.secure_url;
+//     const newPost = new Post({
+//       name,
+//       prompt,
+//       photo: imageUrl,
+//     });
+//     await newPost.save()
+//     res.status(200).json({ success: true, data: newPost });
+//   } catch (err) {
+//     res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
+//   }
+// });
+
+// export default router;
+
 import express from 'express';
 import * as dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+
 import Post from '../mongodb/models/post.js';
 
 dotenv.config();
 
 const router = express.Router();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 router.route('/').get(async (req, res) => {
   try {
@@ -18,26 +72,14 @@ router.route('/').get(async (req, res) => {
 router.route('/').post(async (req, res) => {
   try {
     const { name, prompt, photo } = req.body;
-    const formData = new FormData();
-    formData.append("file", photo);
-    formData.append("upload_preset", "aiimages");
+    const photoUrl = await cloudinary.uploader.upload(photo);
 
-    const response = await fetch(
-      "https://api.cloudinary.com/v1_1/danlppyav/image/upload",
-      {
-        method: "POST",
-        body: formData,
-      }
-    );
-
-    const data = await response.json();
-    const imageUrl = data.secure_url;
-    const newPost = new Post({
+    const newPost = await Post.create({
       name,
       prompt,
-      photo: imageUrl,
+      photo: photoUrl.url,
     });
-    await newPost.save()
+
     res.status(200).json({ success: true, data: newPost });
   } catch (err) {
     res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
